9c0911dbe90bc80308b7131b96877cfb2e64f868
Pete Hatton <pete@monolight.org>
2021-05-15 13:14:04 +0100

    Update method signature - allow nulls to be returned.

--- src/OpcachePreload.php
+++ src/OpcachePreload.php
@@ -3,5 +3,14 @@ namespace ML\Contracts;
 
 interface OpcachePreload
 {
-    public function getFilename(): string;
+    /**
+     * @return string|null
+     *
+     * Returns a expected full absolute path to a PHP opcache.preload file. The file does not have
+     * to exist on the filesystem when calling this method.
+     *
+     * Returns null if there will not be a opache.preload file.
+     *
+     */
+    public function getFilename(): ?string;
 }
